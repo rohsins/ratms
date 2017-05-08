ef62733f4299719fadb45ffdb86378d3dcb8220c
Rohit Singh <rohsins@gmail.com>
2017-05-08 08:39:06 +0545

    revamp

--- ratms.cpp
+++ ratms.cpp
@@ -11,42 +11,75 @@
 #include "math.h"
 
 USART_TypeDef *usartAcc = USART0;
-int16_t axisZ;
-int whoami;
-float deg;
-float zGravity;
-const float toDeg = (180/3.1415);
 
-void blinky(void const* arg) {
-	 while (1) {
-		 GPIO_PinOutSet(gpioPortC, 0);
-		 osDelay(3000);
-		 GPIO_PinOutClear(gpioPortC, 0);
-		 osDelay(10);
-		 GPIO_PinOutSet(gpioPortC, 0);
-		 osDelay(130);
-		 GPIO_PinOutClear(gpioPortC, 0);
-		 osDelay(20);
-	 }
-}
-osThreadDef(blinky, osPriorityNormal, 1, 0);
+uint8_t whoAmI;
+uint8_t axisXL;
+int8_t axisXH;
+int16_t axisX;
+uint8_t axisYL;
+int8_t axisYH;
+int16_t axisY;
+uint8_t axisZL;
+int8_t axisZH;
+int16_t axisZ;
+
+float xGravity;
+float yGravity;
+float zGravity;
+
+enum movement {
+	none = 0,
+	left,
+	backward,
+	right,
+	forward
+};
+
+movement direction;
 
 uint8_t accRead(uint8_t regRead) {
 	uint8_t temp = 0;
 	USART_Tx(usartAcc, (regRead | 0x80));
+	USART_Rx(usartAcc);
 	USART_Tx(usartAcc, 0x00);
+	USART_Tx(usartAcc, (regRead | 0x80));
+	USART_Rx(usartAcc);
+	USART_Tx(usartAcc, 0x00);
+	USART_Tx(usartAcc, (regRead | 0x80));
+	USART_Rx(usartAcc);
+	USART_Tx(usartAcc, 0x00);
+	USART_Tx(usartAcc, (regRead | 0x80));
 	temp = USART_Rx(usartAcc);
+	USART_Tx(usartAcc, 0x00);
 	osDelay(1);
 	return temp;
 }
 
-uint8_t accWrite(uint8_t regWrite, uint8_t regValue) {
-	uint8_t temp = 0;
+void accWrite(uint8_t regWrite, uint8_t regValue) {
 	USART_Tx(usartAcc, regWrite);
 	USART_Tx(usartAcc, regValue);
-	temp = USART_Rx(usartAcc);
 	osDelay(1);
-	return temp;
+}
+
+void readAxisData() {
+	axisXL = accRead(0x28);
+	axisXH = accRead(0x29);
+	axisYL = accRead(0x2A);
+	axisYH = accRead(0x2B);
+	axisZL = accRead(0x2C);
+	axisZH = accRead(0x2D);
+	
+	axisX = axisXH << 8 | axisXL;
+	axisY = axisYH << 8 | axisYL;
+	axisZ = axisZH << 8 | axisZL;
+}
+
+void accConfig() {
+	accWrite(0x20, 0x7F); //enable bdu, zen, data rate 400Hz
+	accWrite(0x24, 0x80); //anti-aliasing filter 400Hz
+	//accWrite(0x23, 0x88); //enable dataready interrupt
+	accWrite(0x23, 0x00);
+	accWrite(0x25, 0x00);
 }
 
 void GPIO_ODD_IRQHandler(void) {
@@ -60,10 +93,10 @@ void GPIO_EVEN_IRQHandler(void) {
 void spiInitialize(void) {
 	USART_InitSync_TypeDef usartInitTypeDefAcc = USART_INITSYNC_DEFAULT;
 	
-	usartInitTypeDefAcc.clockMode = usartClockMode0;
+	usartInitTypeDefAcc.clockMode = usartClockMode3;
 	usartInitTypeDefAcc.msbf = true;
 	
-	usartInitTypeDefAcc.baudrate = 100000;
+	usartInitTypeDefAcc.baudrate = 800000;
 	usartInitTypeDefAcc.databits = usartDatabits8;
 	 
 	USART_Enable_TypeDef usartEnableTypeDef = usartEnable;
@@ -84,76 +117,88 @@ void spiInitialize(void) {
 }
 
 void Initialize(void) {
-	 CHIP_Init();
-	 
-	 CMU_OscillatorEnable (cmuOsc_HFRCO, true, true);
-	 CMU_ClockSelectSet   (cmuClock_HF,    cmuSelect_HFRCO);
-	 CMU_ClockDivSet      (cmuClock_HFPER, cmuClkDiv_1);
-	 
-	 CMU_ClockEnable(cmuClock_HFPER, true);
-	 CMU_ClockEnable(cmuClock_GPIO, true);
-	 CMU_ClockEnable(cmuClock_USART0, true);
-	
-	 GPIO_PinModeSet(gpioPortA, 8, gpioModePushPull, 0); 
-	 GPIO_PinModeSet(gpioPortA, 9, gpioModePushPull, 0); 
-	 GPIO_PinModeSet(gpioPortC, 0, gpioModePushPull, 0); 
-	
-	 spiInitialize();
-	
+	CHIP_Init();
+
+	CMU_OscillatorEnable (cmuOsc_HFRCO, true, true);
+	CMU_ClockSelectSet   (cmuClock_HF,    cmuSelect_HFRCO);
+	CMU_ClockDivSet      (cmuClock_HFPER, cmuClkDiv_1);
+
+	CMU_ClockEnable(cmuClock_HFPER, true);
+	CMU_ClockEnable(cmuClock_GPIO, true);
+	CMU_ClockEnable(cmuClock_USART0, true);
+
+	GPIO_PinModeSet(gpioPortA, 8, gpioModePushPull, 0); 
+	GPIO_PinModeSet(gpioPortA, 9, gpioModePushPull, 0); 
+	GPIO_PinModeSet(gpioPortC, 0, gpioModePushPull, 0); 
+
+	spiInitialize();
+		
 	//gpio interrupts
-	GPIO_PinModeSet(gpioPortD, 6, gpioModeInput, 0);
-	GPIO_PinModeSet(gpioPortD, 7, gpioModeInput, 0);
-	
-	GPIO_IntConfig(gpioPortD, 6, false, true, true);
-	GPIO_IntConfig(gpioPortD, 7, false, true, true);
-	
-	NVIC_ClearPendingIRQ(GPIO_EVEN_IRQn);
-	NVIC_ClearPendingIRQ(GPIO_ODD_IRQn);
-	NVIC_EnableIRQ(GPIO_EVEN_IRQn);
-	NVIC_EnableIRQ(GPIO_ODD_IRQn);
+//	GPIO_PinModeSet(gpioPortD, 6, gpioModeInput, 0);
+//	GPIO_PinModeSet(gpioPortD, 7, gpioModeInput, 0);
+//	
+//	GPIO_IntConfig(gpioPortD, 6, false, true, true);
+//	GPIO_IntConfig(gpioPortD, 7, false, true, true);
+//	
+//	NVIC_ClearPendingIRQ(GPIO_EVEN_IRQn);
+//	NVIC_ClearPendingIRQ(GPIO_ODD_IRQn);
+//	NVIC_EnableIRQ(GPIO_EVEN_IRQn);
+//	NVIC_EnableIRQ(GPIO_ODD_IRQn);
 }
 
-//void outtog(void const* arg) {
-//	 while (1) {
-//		 GPIO_PinOutSet(gpioPortA, 8);
-//		 GPIO_PinOutClear(gpioPortA, 9);
-//		 osDelay(3000);
-//		 GPIO_PinOutClear(gpioPortA, 8);
-//		 GPIO_PinOutSet(gpioPortA, 9);
-//		 osDelay(3000);
-//	 }
-//}
-//osThreadDef(outtog, osPriorityNormal, 1, 0);
+void blinky(void const* arg) {
+	 while (1) {
+		 GPIO_PinOutSet(gpioPortC, 0);
+		 osDelay(3000);
+		 GPIO_PinOutClear(gpioPortC, 0);
+		 osDelay(10);
+		 GPIO_PinOutSet(gpioPortC, 0);
+		 osDelay(130);
+		 GPIO_PinOutClear(gpioPortC, 0);
+		 osDelay(20);
+	 }
+}
+osThreadDef(blinky, osPriorityBelowNormal, 1, 0);
 
 void runner(void const * params) {	
-	accWrite(0x20, 0x7C); //enable bdu, zen, data rate 400Hz
-	accWrite(0x24, 0x80); //anti-aliasing filter 400Hz
-	accWrite(0x23, 0x88); //enable dataready interrupt
-	
-	int8_t axisZH;
-	uint8_t axisZL;
-	
 	while (1) {
-		whoami = accRead(0x0F);
-		axisZH = accRead(0x2D);
-		axisZL = accRead(0x2C);
-		axisZ = axisZH  << 8 | axisZL;
-		zGravity = ((axisZ*2.0)/32768.0);
+		whoAmI = accRead(0x0F);
+		if (whoAmI == 0x3F) readAxisData();
 		osDelay(100);
 	}
 }
 osThreadDef(runner, osPriorityNormal, 1, 0);
 
+void computeEngine(void const * params) {
+	while (1) {
+		if (axisX > 4000) direction = forward;
+		else if (axisX < -6000) direction = backward;
+		else if (axisY > 6000) direction = left;
+		else if (axisY < -6000) direction = right;
+		else direction = none;
+		
+		xGravity = (axisX * 9.8)/16384;
+		yGravity = (axisY * 9.8)/16384;
+		zGravity = (axisZ * 9.8)/16384;
+		
+		osDelay(200);
+	}
+}
+osThreadDef(computeEngine, osPriorityNormal, 1, 0);
+
  int main (void) {
-  SystemCoreClockUpdate();
+	 
 	SysTick_Config(SystemCoreClock/1000); // 1 milisecond SysTick
 	
 	osKernelInitialize();
+	osKernelStart();
 	Initialize();
+	accConfig();
 		
 	osThreadCreate(osThread(blinky), NULL);
 //	osThreadCreate(osThread(outtog), NULL);
 	osThreadCreate(osThread(runner), NULL);
+	osThreadCreate(osThread(computeEngine), NULL);
 	
 	return 0;
 }
